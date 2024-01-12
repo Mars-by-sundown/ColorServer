cb3b3b3f9b8e13149b37d54f26b9871fa84403b1
Nicholas Ragano <nragano@gmail.com>
2024-01-12 11:19:11 -0600

    initial commit

---
+++ ColorServer/src/ColorServer.java
@@ -0,0 +1,33 @@
+/*
+1. Name: Nicholas Ragano
+
+2. Date: 2024-01-12
+
+3. Java version: 21.0.1
+
+4. Precise command-line compilation examples / instructions: 
+
+    > javac ColorServer.java 
+
+5. Precise examples / instructions to run this program:
+
+    In separate shell windows:
+
+        > java ColorServer
+        > java ColorClient
+
+6. Full list of files needed for running the program: 
+
+ a. ColorServer.java
+
+7. Notes: 
+
+
+ */
+
+
+public class ColorServer {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
